56e19fb37118b12f997b5374e404bfa40e35e65d
sT31_N <st31n.tech@gmail.com>
2021-07-15 17:21:58 -0500

    Final Version

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 const URL = 'localhost:8080'; // String Holding the URL for the App
 
 // External Modules
-const express = require('express');
-const path = require('path');
-const app = new express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require('express');        // An external package built into nodejs. Used to build the backend
+const path = require('path');              // An external javascript package allowing us to use file paths
+const bodyParser = require('body-parser'); // An external package used to parse data from html bodies
+const fs = require('fs');                  // An external package built into nodejs, which manipulates the files
 
 // Internal Module
 const message = require('./message');
 
 // Begin Server Startup
 console.log('Server Starting...');
+const app = new express(); 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: false
@@ -30,18 +30,40 @@ app.get('/', function(request, response){
         content += message.GetMessages()[0] + '|#|';
         content += message.GetMessages()[1] + '|#|';
         content += message.GetMessages()[2] + '|#|';
-        // fs.writeFileSync('./files/temp/messages.xml', content);
+
         response.setHeader('Content-Type', 'text/plain');
         response.setHeader('Content-Disposition', 'attachment; filename=messages');
         response.write(content, 'utf-8');
         response.end();
     });
 
+// Save the message to memory
 app.post('/submitPost', function(request, response){
     message.Push(request.body.author, request.body.code, request.body.summary);
     response.writeHead(301, { "Location": "http://" + URL + '/' });
     return response.end();
 });
+
+let output = ''; // Variable used to store the output of the file run on the frontend
+app.post('/runFile', function(request, response){ // Called when a user runs the code in a message
+    // Create a temporary function.
+    let func = new Function(message.GetMessages()[1][request.body.messageID]);
+    // Stores the output 
+    output = JSON.stringify(func());
+    // Redirect
+    response.writeHead(301, { "Location": "http://" + URL + '/consoleOutput' });
+    return response.end();
+});
+// Used to show the console output
+app.get('/consoleOutput', function(request, response){
+    response.sendFile(path.resolve('./consoleOut.html'));
+});
+    app.get('/getOutput', function(request, response){
+        response.setHeader('Content-Type', 'text/plain');
+        response.setHeader('Content-Disposition', 'attachment; filename=messages');
+        response.write(output, 'utf-8');
+        response.end();
+    });
 
 // Finish Server Startup
 app.listen(8080);
--- message.js
+++ message.js
@@ -1,17 +1,20 @@
-// Variables storing data for all posts
+// Variables storing data for all posts/messages
 let mAuthor = [];
 let mCode = [];
 let mSummary = [];
 
+// Store data for a post/message
 function Push(author, code, summary) {
     mAuthor.push(author);
     mCode.push(code);
     mSummary.push(summary);
 }
 
+// Get data on all posts/messages
 function GetMessages(){
     return [mAuthor, mCode, mSummary];
 }
 
+// Export functions to module
 module.exports.Push = Push;
 module.exports.GetMessages = GetMessages;
